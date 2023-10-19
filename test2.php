5d9a97f3a47fa22fc122aca50c2ebc0c8f581b0a
SergEtalon-bt <130131848+SergEtalon-bt@users.noreply.github.com>
2023-10-19 17:26:52 +0300

    Create test2.php

---
+++ test2.php
@@ -0,0 +1,12 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-5">
+            <div id="templateproduct">
+                <product-edit data='<?= $arData['data']; ?>'></product-edit>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+
+<script type="text/javascript" src="../templates/products/js/editProduct.js"></script>
